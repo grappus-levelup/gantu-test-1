340aab6c4e51658541e79e158af49727f209b886
grappus-levelup <levelup@grappus.com>
2025-05-29 16:55:23 +0530

    Restore original font configuration

--- lib/theme/text_style_helper.dart
+++ lib/theme/text_style_helper.dart
@@ -13,175 +13,402 @@ class TextStyleHelper {
   }
 
   // Headline Styles
+  // Medium-large text styles for section headers
+
   TextStyle get headline30Medium => TextStyle(
-        fontFamily: 'Inter',
         fontSize: 30.fSize,
         fontWeight: FontWeight.w500,
         color: appTheme.blackCustom,
       );
 
   // Title Styles
+  // Medium text styles for titles and subtitles
+
   TextStyle get title22Medium => TextStyle(
-        fontFamily: 'Inter',
         fontSize: 22.fSize,
         fontWeight: FontWeight.w500,
         color: appTheme.whiteCustom,
       );
 
   TextStyle get title20RegularRoboto => TextStyle(
-        fontFamily: 'Inter',
         fontSize: 20.fSize,
         fontWeight: FontWeight.w400,
+        fontFamily: 'Roboto',
       );
 
   TextStyle get title20Medium => TextStyle(
-        fontFamily: 'Inter',
         fontSize: 20.fSize,
         fontWeight: FontWeight.w500,
         color: appTheme.whiteCustom,
       );
 
   TextStyle get title20SemiBold => TextStyle(
-        fontFamily: 'Inter',
         fontSize: 20.fSize,
         fontWeight: FontWeight.w600,
         color: appTheme.blackCustom,
       );
 
   TextStyle get title18Regular => TextStyle(
-        fontFamily: 'Inter',
         fontSize: 18.fSize,
         fontWeight: FontWeight.w400,
         color: appTheme.whiteCustom,
       );
 
   TextStyle get title18Medium => TextStyle(
-        fontFamily: 'Inter',
         fontSize: 18.fSize,
         fontWeight: FontWeight.w500,
         color: appTheme.blackCustom,
       );
 
   TextStyle get title16Regular => TextStyle(
-        fontFamily: 'Inter',
         fontSize: 16.fSize,
         fontWeight: FontWeight.w400,
         color: appTheme.whiteCustom,
       );
 
-  TextStyle get title16 => TextStyle(
-        fontFamily: 'Inter',
-        fontSize: 16.fSize,
-        color: appTheme.whiteCustom,
-      );
+  TextStyle get title16 =>
+      TextStyle(fontSize: 16.fSize, color: appTheme.whiteCustom);
 
   TextStyle get title16Medium => TextStyle(
-        fontFamily: 'Inter',
         fontSize: 16.fSize,
         fontWeight: FontWeight.w500,
         color: appTheme.blackCustom,
       );
 
   // Body Styles
+  // Standard text styles for body content
+
   TextStyle get body15SemiBold => TextStyle(
-        fontFamily: 'Inter',
         fontSize: 15.fSize,
         fontWeight: FontWeight.w600,
         color: appTheme.blackCustom,
       );
 
   TextStyle get body14Regular => TextStyle(
-        fontFamily: 'Inter',
         fontSize: 14.fSize,
         fontWeight: FontWeight.w400,
         color: appTheme.whiteCustom,
       );
 
   TextStyle get body14SemiBold => TextStyle(
-        fontFamily: 'Inter',
         fontSize: 14.fSize,
         fontWeight: FontWeight.w600,
         color: appTheme.blackCustom,
       );
 
   TextStyle get body14Medium => TextStyle(
-        fontFamily: 'Inter',
         fontSize: 14.fSize,
         fontWeight: FontWeight.w500,
         color: appTheme.colorFF1F29,
       );
 
   TextStyle get body14Light => TextStyle(
-        fontFamily: 'Inter',
         fontSize: 14.fSize,
         fontWeight: FontWeight.w300,
         color: appTheme.blackCustom,
       );
 
-  TextStyle get body14 => TextStyle(
-        fontFamily: 'Inter',
-        fontSize: 14.fSize,
-        color: appTheme.blackCustom,
-      );
+  TextStyle get body14 =>
+      TextStyle(fontSize: 14.fSize, color: appTheme.blackCustom);
 
   TextStyle get body12Regular => TextStyle(
-        fontFamily: 'Inter',
         fontSize: 12.fSize,
         fontWeight: FontWeight.w400,
         color: appTheme.whiteCustom,
       );
 
   TextStyle get body12Medium => TextStyle(
-        fontFamily: 'Inter',
         fontSize: 12.fSize,
         fontWeight: FontWeight.w500,
         color: appTheme.whiteCustom,
       );
 
   TextStyle get body12SemiBold => TextStyle(
-        fontFamily: 'Inter',
         fontSize: 12.fSize,
         fontWeight: FontWeight.w600,
         color: appTheme.blackCustom,
       );
 
-  TextStyle get body12 => TextStyle(
-        fontFamily: 'Inter',
-        fontSize: 12.fSize,
-        color: appTheme.whiteCustom,
-      );
+  TextStyle get body12 =>
+      TextStyle(fontSize: 12.fSize, color: appTheme.whiteCustom);
 
   TextStyle get body12Light => TextStyle(
-        fontFamily: 'Inter',
         fontSize: 12.fSize,
         fontWeight: FontWeight.w300,
         color: appTheme.blackCustom,
       );
 
   // Label Styles
+  // Small text styles for labels, captions, and hints
+
   TextStyle get label11Light => TextStyle(
-        fontFamily: 'Inter',
         fontSize: 11.fSize,
         fontWeight: FontWeight.w300,
         color: appTheme.blackCustom,
       );
 
-  TextStyle get label11 => TextStyle(
-        fontFamily: 'Inter',
-        fontSize: 11.fSize,
-        color: appTheme.blackCustom,
-      );
+  TextStyle get label11 =>
+      TextStyle(fontSize: 11.fSize, color: appTheme.blackCustom);
 
   TextStyle get label9SemiBold => TextStyle(
-        fontFamily: 'Inter',
         fontSize: 9.fSize,
         fontWeight: FontWeight.w600,
         color: appTheme.blackCustom,
       );
 
-  TextStyle get label9 => TextStyle(
-        fontFamily: 'Inter',
-        fontSize: 9.fSize,
-        color: appTheme.whiteCustom,
-      );
+  TextStyle get label9 =>
+      TextStyle(fontSize: 9.fSize, color: appTheme.whiteCustom);
+
+
+  // Add these styles to TextStyleHelper class
+
+// Inter Light (300) Styles
+  TextStyle get inter30Light => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 30.fSize,
+    fontWeight: FontWeight.w300,
+    color: appTheme.blackCustom,
+  );
+
+  TextStyle get inter22Light => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 22.fSize,
+    fontWeight: FontWeight.w300,
+    color: appTheme.blackCustom,
+  );
+
+  TextStyle get inter20Light => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 20.fSize,
+    fontWeight: FontWeight.w300,
+    color: appTheme.blackCustom,
+  );
+
+  TextStyle get inter18Light => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 18.fSize,
+    fontWeight: FontWeight.w300,
+    color: appTheme.blackCustom,
+  );
+
+  TextStyle get inter16Light => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 16.fSize,
+    fontWeight: FontWeight.w300,
+    color: appTheme.blackCustom,
+  );
+
+  TextStyle get inter14Light => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 14.fSize,
+    fontWeight: FontWeight.w300,
+    color: appTheme.blackCustom,
+  );
+
+  TextStyle get inter12Light => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 12.fSize,
+    fontWeight: FontWeight.w300,
+    color: appTheme.blackCustom,
+  );
+
+// Inter Regular (400) Styles
+  TextStyle get inter30Regular => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 30.fSize,
+    fontWeight: FontWeight.w400,
+    color: appTheme.blackCustom,
+  );
+
+  TextStyle get inter22Regular => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 22.fSize,
+    fontWeight: FontWeight.w400,
+    color: appTheme.blackCustom,
+  );
+
+  TextStyle get inter20Regular => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 20.fSize,
+    fontWeight: FontWeight.w400,
+    color: appTheme.blackCustom,
+  );
+
+  TextStyle get inter18Regular => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 18.fSize,
+    fontWeight: FontWeight.w400,
+    color: appTheme.blackCustom,
+  );
+
+  TextStyle get inter16Regular => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 16.fSize,
+    fontWeight: FontWeight.w400,
+    color: appTheme.blackCustom,
+  );
+
+  TextStyle get inter14Regular => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 14.fSize,
+    fontWeight: FontWeight.w400,
+    color: appTheme.blackCustom,
+  );
+
+  TextStyle get inter12Regular => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 12.fSize,
+    fontWeight: FontWeight.w400,
+    color: appTheme.blackCustom,
+  );
+
+// Inter Medium (500) Styles
+  TextStyle get inter30Medium => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 30.fSize,
+    fontWeight: FontWeight.w500,
+    color: appTheme.blackCustom,
+  );
+
+  TextStyle get inter22Medium => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 22.fSize,
+    fontWeight: FontWeight.w500,
+    color: appTheme.blackCustom,
+  );
+
+  TextStyle get inter20Medium => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 20.fSize,
+    fontWeight: FontWeight.w500,
+    color: appTheme.blackCustom,
+  );
+
+  TextStyle get inter18Medium => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 18.fSize,
+    fontWeight: FontWeight.w500,
+    color: appTheme.blackCustom,
+  );
+
+  TextStyle get inter16Medium => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 16.fSize,
+    fontWeight: FontWeight.w500,
+    color: appTheme.blackCustom,
+  );
+
+  TextStyle get inter14Medium => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 14.fSize,
+    fontWeight: FontWeight.w500,
+    color: appTheme.blackCustom,
+  );
+
+  TextStyle get inter12Medium => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 12.fSize,
+    fontWeight: FontWeight.w500,
+    color: appTheme.blackCustom,
+  );
+
+// Inter SemiBold (600) Styles
+  TextStyle get inter30SemiBold => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 30.fSize,
+    fontWeight: FontWeight.w600,
+    color: appTheme.blackCustom,
+  );
+
+  TextStyle get inter22SemiBold => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 22.fSize,
+    fontWeight: FontWeight.w600,
+    color: appTheme.blackCustom,
+  );
+
+  TextStyle get inter20SemiBold => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 20.fSize,
+    fontWeight: FontWeight.w600,
+    color: appTheme.blackCustom,
+  );
+
+  TextStyle get inter18SemiBold => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 18.fSize,
+    fontWeight: FontWeight.w600,
+    color: appTheme.blackCustom,
+  );
+
+  TextStyle get inter16SemiBold => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 16.fSize,
+    fontWeight: FontWeight.w600,
+    color: appTheme.blackCustom,
+  );
+
+  TextStyle get inter14SemiBold => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 14.fSize,
+    fontWeight: FontWeight.w600,
+    color: appTheme.blackCustom,
+  );
+
+  TextStyle get inter12SemiBold => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 12.fSize,
+    fontWeight: FontWeight.w600,
+    color: appTheme.blackCustom,
+  );
+
+// Inter Bold (700) Styles
+  TextStyle get inter30Bold => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 30.fSize,
+    fontWeight: FontWeight.w700,
+    color: appTheme.blackCustom,
+  );
+
+  TextStyle get inter22Bold => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 22.fSize,
+    fontWeight: FontWeight.w700,
+    color: appTheme.blackCustom,
+  );
+
+  TextStyle get inter20Bold => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 20.fSize,
+    fontWeight: FontWeight.w700,
+    color: appTheme.blackCustom,
+  );
+
+  TextStyle get inter18Bold => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 18.fSize,
+    fontWeight: FontWeight.w700,
+    color: appTheme.blackCustom,
+  );
+
+  TextStyle get inter16Bold => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 16.fSize,
+    fontWeight: FontWeight.w700,
+    color: appTheme.blackCustom,
+  );
+
+  TextStyle get inter14Bold => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 14.fSize,
+    fontWeight: FontWeight.w700,
+    color: appTheme.blackCustom,
+  );
+
+  TextStyle get inter12Bold => TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 12.fSize,
+    fontWeight: FontWeight.w700,
+    color: appTheme.blackCustom,
+  );
 }
